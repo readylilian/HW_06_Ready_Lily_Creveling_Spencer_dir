08ea4a19c4b19dfd03fea513458571e325df6211
Lilian Ready <dezingerlily@gmail.com>
2024-10-30 17:44:46 -0400

    Added some extra lines to get the info we needed, and moved the dendrogram stuff to part b

--- HW_06_Ready_Lily_Creveling_Spencer.m
+++ HW_06_Ready_Lily_Creveling_Spencer.m
@@ -6,16 +6,17 @@ function HW_06_Ready_Lily_Creveling_Spencer(file_in)
     data = removevars(data,"ID");
     % Run Part A
     ccc_pack_mat = Part_A(data);
-    % Add nice labels to the columns and rows for east comparison
+    % Add nice labels to the columns and rows for easy comparison
     absolute = abs(ccc_pack_mat);
     labels = data.Properties.VariableNames(:);
     abs_labeled = array2table(absolute,'VariableNames',labels);
     abs_labeled.Properties.RowNames = labels(:);
     ccc_labeled = array2table(ccc_pack_mat,'VariableNames',labels);
     ccc_labeled.Properties.RowNames = labels(:);
-    ccc_labeled;
+    % Run Part A
+    data = table2array(data);
+    Part_B(data);
     %create dendogram
-    dendogram = Dendrogram(file_in);
 end
 
 % Completes part A of the assignment
@@ -34,23 +35,13 @@ end
 % Uses our built agglomeration clustering to group the data into clusters
 % using the manhattan distance and the center of mass to determine distance
 % and linkage
-function Part_B(file_in)
+function Part_B(data)
     
-
-end
-
-function dendrogram = Dendrogram(file_in)
     % Cluster the guests into groups as follows:
     % a. At the start of agglomerative clustering, assign each record to 
     % its own cluster prototype. Suppose we have 1000+ records. So, you 
     % start with 1000-plus clusters and 1000-plus prototypes of those 
     % clusters.
-    
-    %read in file
-    data = readtable(file_in);
-    % remove the Record IDs
-    data = removevars(data,"ID");
-    data = table2array(data);
 
     %creates a new cluster for each customer
     clusters = {};
@@ -58,38 +49,86 @@ function dendrogram = Dendrogram(file_in)
         clusters = [clusters, CustomerCluster(data(customerId,:),customerId)];
     end
 
+    % creates a rough dendrogram based off the clusters. this was used for
+    % part b of the writeup and does not actually need to be in the final
+    % assignment if we change how Dendrogram works
+    Dendrogram(clusters);
     %loop through all  rows and add them to a cluster
   
-    % b. Use the Manhattan distance between cluster centers as the distance metric.
-    
-    % c. Use the center of mass as the prototype center, the center of mass
-    % of a set of records, to represent its center location in data space. 
     % And use the distance between these centers as the linkage method.
     
     % d. Note: At each step of clustering, two clusters are merged together.
     % Track the size of the smallest of the two clusters that are merged together.
 
+    smallest = [];
     for clusterCount = 1:width(clusters)-1
         %extract the center from all clusters
+        % c. Use the center of mass as the prototype center, the center of mass
+        % of a set of records, to represent its center location in data space.  
         centers = vertcat(clusters.center);
     
         %calculate the distances
+        %Use the Manhattan distance between cluster centers as the distance metric.
         distances = pdist2(centers,centers,'cityblock');
     
-        %mask out the diagonla so we dont pick it
+        %mask out the diagonal so we dont pick it
         distances(logical(eye(size(distances)))) = Inf;
     
-        %pick the id of the 2 cloest customer (can be speed up)
+        %pick the id of the 2 closest customer (can be speed up)
         min_dist = min(distances,[],'all');
         [customer_id_left,customer_id_right] = find(distances == min_dist,1);
     
         %merge the right customer custer into the left customer cluster
         clusterLeft = clusters(customer_id_left);
         clusterRight = clusters(customer_id_right);
+        
+        % Track the size of the smallest of the two clusters that are 
+        % merged together.
+
+        if size(clusterLeft.members,1) < size(clusterRight.members,1)
+            smallest = cat(1,smallest,[size(clusterLeft.members,1)]);
+        else
+            smallest = cat(1,smallest,[size(clusterRight.members,1)]);
+        end
+
         clusterLeft.mergeCluster(clusterRight);
         
         %delete the right cluster as it has been moerged away
         clusters(customer_id_right) = [];
     end
-    dendrogram = clusters(1);
+    clusters(1);
+    % Find the centroids of the four clusters
+    parent = clusters(1).children;
+    cent_one = Find_Centroid(parent{1,1}.children{1,1});
+    cent_two = Find_Centroid(parent{1,1}.children{1,2});
+    cent_three = Find_Centroid(parent{1,2}.children{1,1});
+    cent_four = Find_Centroid(parent{1,2}.children{1,2});
+end
+
+% Given a cluster find the centroid
+function centroid = Find_Centroid(cluster)
+    center = cluster.center;
+    smallest = [-1  Inf];
+    % Look through all points
+    for index = 1:size(cluster.members,1)
+        % Check the distance to the center
+        dist = pdist2(center,cluster.members(index,:));
+        % If it's closest, store it as the centroid
+        if dist < smallest(:,2)
+            smallest = [index dist];
+        end
+    end
+    % Return the item that was found to be closest
+    centroid = cluster.members(smallest(:,1),:); 
+end
+
+% THIS IS A ROUGH DENDROGRAM, IT HAS THE THE THREE SPLIT THING
+% EVERYTHING IN HERE CAN BE DELETED
+function dendro = Dendrogram(clusters)
+    figure;
+    centers = vertcat(clusters.center);
+    distances = pdist2(centers,centers,'cityblock');
+    link = linkage(distances,"centroid");
+    leaforder = optimalleaforder(link,distances);
+    dendro = dendrogram(link,20,Reorder=leaforder);
 end
